0065ba757a2735d9ad6cc8a11ea588611a1687ab
Akansha Jain <jakansha2001@gmail.com>
2020-07-21 02:38:41 +0530

    Adding new project

---
+++ PaniNo2/app/src/main/java/com/example/panino/ContactsPage.java
@@ -0,0 +1,14 @@
+package com.example.panino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_page);
+    }
+}
